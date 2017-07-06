06dd916b09e68a2a4ae12c8613956b0e1d26b2c7
Jennifer Pemberton <jpemberton76@gmail.com>
2017-07-05 20:34:33 -0400

    basic app working

---
+++ src/reducers/reducer_active_book.js
@@ -0,0 +1,11 @@
+// State argument is not application state, only the state
+//this reducer is responsible for
+//default value of state to null, if it is undefined
+export default function(state = null, action) {
+  switch(action.type) {
+    case 'BOOK_SELECTED':
+      return action.payload;
+  }
+
+  return state;
+}
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+  return [
+    { title: 'Javascript: The Good Parts', pages: 200 },
+    { title: 'Harry Potter', pages: 180 },
+    { title: 'The Women in the Castle', pages: 300 },
+    { title: 'The Passage', pages: 500 }
+  ]
+}
